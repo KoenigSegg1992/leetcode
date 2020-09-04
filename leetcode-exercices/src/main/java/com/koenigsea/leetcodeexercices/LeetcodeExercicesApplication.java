9df5f89636e02bbdaf6804c368b44e7ae0db7f60
zhouyi <zhouyia@tfzq.com>
2020-09-04 15:22:02 +0800

    init

---
+++ leetcode-exercices/src/main/java/com/koenigsea/leetcodeexercices/LeetcodeExercicesApplication.java
@@ -0,0 +1,13 @@
+package com.koenigsea.leetcodeexercices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeExercicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetcodeExercicesApplication.class, args);
+    }
+
+}
